c3c0fff6b1abe58b0872322ef0b05cc66dfef2ec
Si <siwang22@gmail.com>
2016-03-02 22:59:31 -0800

    (feat) Generate board info

--- mainController.js
+++ mainController.js
@@ -1,8 +1,26 @@
 angular.module('mainCtrl', [])
   .controller('mainController', ['$scope', function($scope) {
   
+    $scope.spaces = [
+
+    ];
+
+    $scope.determineSpaces = function(n) {
+      for(var row = 0; row < n; row++) {
+        for(var column = 0; column < n; column++) {
+          $scope.spaces.push({
+            row: row,
+            column: column
+          });
+        }
+      }
+    };
+
     $scope.newGame = function() {
       //TODO
     };
+
+    $scope.determineSpaces(3);
+    console.log($scope.spaces);
 
   }]);
