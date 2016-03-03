114c0f20baf083b02501f4a4a60266c93aacdd98
Si <siwang22@gmail.com>
2016-03-02 23:51:40 -0800

    (feat) Implement virtual board to keep track of marked spaces

--- mainController.js
+++ mainController.js
@@ -1,18 +1,26 @@
 angular.module('mainCtrl', [])
   .controller('mainController', ['$scope', function($scope) {
   
-    $scope.spaces = [
+    //An array of objects. Each object pertains to a space 
+    //on the board, containing the row and column property
+    $scope.spaces = [];
 
-    ];
+    //An array to keep track of marked spaces.
+    //0 for unmarked, -1 for X's, and 1 for O's
+    $scope.virtualBoard = [];
 
+    //Populate spaces for both the spaces array and virtualBoard array
     $scope.determineSpaces = function(n) {
       for(var row = 0; row < n; row++) {
+        var currentRow = [];
         for(var column = 0; column < n; column++) {
           $scope.spaces.push({
             row: row,
             column: column
           });
+          currentRow.push(0);
         }
+        $scope.virtualBoard.push(currentRow);
       }
     };
 
@@ -22,5 +30,6 @@ angular.module('mainCtrl', [])
 
     $scope.determineSpaces(3);
     console.log($scope.spaces);
+    console.log($scope.virtualBoard);
 
   }]);
