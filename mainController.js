8edc8c404a26d5daf504b6ea731837bbc519e481
Si <siwang22@gmail.com>
2016-03-03 16:44:46 -0800

    (feat) Begin logic for checking end game scenarios

--- mainController.js
+++ mainController.js
@@ -11,6 +11,10 @@ angular.module('mainCtrl', [])
     //0 for unmarked, -1 for X's, and 1 for O's
     $scope.virtualBoard = [];
 
+    $scope.turns = 0;
+
+    $scope.boardSize = 3;
+
     //Populate spaces for both the spaces array and virtualBoard array
     $scope.determineSpaces = function(n) {
       for(var row = 0; row < n; row++) {
@@ -30,23 +34,54 @@ angular.module('mainCtrl', [])
       //TODO
     };
 
+    $scope.checkWin = function(row, column) {
+      console.log('checkWin called');
+    };
+
+    $scope.winScenario = function() {
+
+    };
+
+    $scope.tieScenario = function() {
+
+    };
+
     $scope.clickSpace = function($event, row, column) {
       //Change class to x or o
       //update virtual board
       //Check for win scenario
       //Check if board is filled, if so then go to tie
       if($scope.virtualBoard[row][column] === 0) {
+        $scope.turns++;
+        
+        var space = $event.currentTarget;
+        
         if($scope.turn === 'x') {
           $scope.virtualBoard[row][column] = -1;
+          $(space).addClass('x');
+          $scope.turn = 'o';
         } else {
           $scope.virtualBoard[row][column] = 1;
+          $(space).addClass('o');
+          $scope.turn = 'x';
         }
-        var space = $event.currentTarget;
+
+        //Check if a win is possible
+        if($scope.turns >= 5) {
+          //Check if a win occured
+          if($scope.checkWin(row, column)) {
+            $scope.winScenario();
+            //Check if 
+          } else if($scope.turns === $scope.boardSize * $scope.boardSize) {
+            $scope.tieScenario();
+          }
+        }
+
         console.log('click space logic reached');
       }
     };
 
-    $scope.determineSpaces(3);
+    $scope.determineSpaces($scope.boardSize);
     console.log($scope.spaces);
     console.log($scope.virtualBoard);
 
